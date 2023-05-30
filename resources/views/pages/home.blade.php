92910d841fa16b1763f4aab7f490b2743e0ee08b
ilham06 <ilham06@gmail.com>
2023-05-30 15:16:01 +0700

    setup

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,12 @@
+@include('layouts.main')
+
+@section('content')
+    <table class="table">
+        <thead class="table-dark">
+            ...
+        </thead>
+        <tbody>
+            ...
+        </tbody>
+    </table>
+@endsection
